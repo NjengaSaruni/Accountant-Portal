d96ce2d37e9be6288ce96b47ff50bc91c1dd893c
Peter Njenga <njengasaruni@gmail.com>
2018-12-05 20:48:15 +0300

    Side bar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {MatButtonModule, MatSidenavModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatButtonModule,
+        MatSidenavModule,
+        BrowserAnimationsModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +25,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'accountant-portal'`, () => {
+  it(`should have as title 'Accountant | Home'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('accountant-portal');
+    expect(app.title).toEqual('Accountant | Home');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to accountant-portal!');
+    expect(compiled.querySelector(
+      'body > app-root > mat-sidenav-container > mat-sidenav-content > app-home > div.top-header > app-header > div > div:nth-child(2)')
+      .textContent).toContain('this');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -15,9 +13,7 @@ import {MatButtonModule} from '@angular/material';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    MatButtonModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
