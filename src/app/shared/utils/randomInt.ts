84a9b96e4f5cc82624402fa76e0b9492a1f1e29d
Peter Njenga <njengasaruni@gmail.com>
2018-12-07 20:24:43 +0300

    Random int utility function with JSdoc

---
+++ src/app/shared/utils/randomInt.ts
@@ -0,0 +1,12 @@
+/**
+ * @description
+ * A utility method that returns a random integer
+ * between min (inclusive) and max (exclusive)
+ * generate a random integer between min and max
+ * @param min Mininum number exclusive
+ * @param max Maximum number exclusive
+ * @return random generated integer
+ */
+export function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
