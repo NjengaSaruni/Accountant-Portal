2e9e289f406b6d2e6bea98627a9434d173dd5085
Peter Njenga <njengasaruni@gmail.com>
2018-12-07 10:41:20 +0300

    Proper directory structure for project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
-import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './shared/components/header/header.component';
+import {HomeComponent} from './modules/home/home.component';
 import {AngularFontAwesomeModule} from 'angular-font-awesome';
 
 @NgModule({
