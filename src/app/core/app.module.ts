55ab7bec76919ef4ef6e153d7c8e426339ee2757
Peter Njenga <njengasaruni@gmail.com>
2018-12-13 23:58:16 +0300

    Working material-like loader

--- src/app/core/app.module.ts
+++ src/app/core/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {WindowRefService} from '../common/services/window-ref.service';
+import {SharedModule} from '../modules/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {WindowRefService} from '../common/services/window-ref.service';
     BrowserModule,
 
     // Internal modules
+    SharedModule,
 
     // Primary routing module
     AppRoutingModule,
