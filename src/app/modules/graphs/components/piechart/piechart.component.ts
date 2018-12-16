9b78829d981213b667e137f023798595a4a3456c
Peter Njenga <njengasaruni@gmail.com>
2018-12-16 23:59:42 +0300

    SVG component

--- src/app/modules/graphs/components/piechart/piechart.component.ts
+++ src/app/modules/graphs/components/piechart/piechart.component.ts
@@ -1,18 +1,27 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {BarGraph} from '../../../../common/models/graphs/BarGraph';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-piechart',
   templateUrl: './piechart.component.html',
   styleUrls: ['./piechart.component.scss']
 })
-export class PiechartComponent implements OnInit {
-  @Input() graph: BarGraph;
+export class PiechartComponent implements OnInit, AfterViewInit {
   @Input() max: number;
+  @ViewChild('svg') public svg: ElementRef;
+
+  getCoordinatesForPercent(percent) {
+    const x = Math.cos(2 * Math.PI * percent);
+    const y = Math.sin(2 * Math.PI * percent);
+
+    return [x, y];
+  }
 
   constructor(
   ) { }
 
+  public ngAfterViewInit() {
+    this.svg = HTMLs
+  }
   ngOnInit(
   ) {
   }
