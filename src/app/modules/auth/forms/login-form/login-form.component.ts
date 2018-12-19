205d2eb354ed3914a425b05ca8380a0b6f38a8f0
Peter Njenga <njengasaruni@gmail.com>
2018-12-19 23:51:20 +0300

    Form validation for login, more styling for login and content pages

--- src/app/modules/auth/forms/login-form/login-form.component.ts
+++ src/app/modules/auth/forms/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {LoaderService} from '../../../shared/components/loader/loader.service';
 
 @Component({
   selector: 'app-login-form',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent implements OnInit {
+  loginForm: FormGroup;
+  formLabels: any = {email: false, password: false};
 
-  constructor() { }
+
+  constructor(private formBuilder: FormBuilder,
+              private loaderService: LoaderService) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl(
+        '',
+        [
+          Validators.email,
+          Validators.required
+        ]
+      ),
+      password: new FormControl(
+        '',
+        [
+          Validators.required
+        ]
+      ),
+    });
   }
 
+  signIn() {
+    this.loaderService.show();
+    if (this.loginForm.invalid) {
+      return;
+    }
+  }
 }
