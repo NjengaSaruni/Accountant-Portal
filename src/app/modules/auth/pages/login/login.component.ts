157c245028a71931556bbd09211aef4ab48c4e1f
Peter Njenga <njengasaruni@gmail.com>
2018-12-22 01:35:12 +0300

    Working message service closes #29

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import * as fromStore from '../../../../common/store';
+import {Message} from '../../../../common/models/messages/Message';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +11,12 @@ import * as fromStore from '../../../../common/store';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  messages$: Observable<Message[]>;
 
   constructor(private store: Store<fromStore.ReportsState>) { }
 
   ngOnInit() {
-    this.store.select(fromStore.getAllMessages).subscribe(
-      state => console.log(state),
-    );
+    this.messages$ = this.store.select(fromStore.getAllMessages);
   }
 
 }
