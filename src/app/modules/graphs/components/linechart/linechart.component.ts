95d57b1ce3a86a03c5adbf2fd82e716ed6fbf4dc
Peter Njenga <njengasaruni@gmail.com>
2018-12-25 21:16:53 +0300

    Self adjusting line graph

--- src/app/modules/graphs/components/linechart/linechart.component.ts
+++ src/app/modules/graphs/components/linechart/linechart.component.ts
@@ -28,18 +28,19 @@ export class LinechartComponent implements OnInit, AfterViewInit {
 
     this.cx.fillStyle = '#f6f6f6';
     this.cx.fillRect(0, 0, canvasEl.width, canvasEl.height);
-    const data = [40, 65, 72, 120, 250, 87, 100, 42];
+    const data = [40, 65, 72, 120, 250, 87, 100, 42, 90, 11, 21, 122, 12, 123, 2, 12, 3, 122, 12, 221];
+    const toMoveWith = canvasEl.width / data.length;
     let left =  0;
     let prev = data[0];
     for (let i = 0; i < data.length; i++) {
       this.cx.beginPath();
       this.cx.moveTo(left, prev);
-      this.cx.lineTo(left + 100, data[i]);
+      this.cx.lineTo(left + toMoveWith, data[i]);
       this.cx.lineWidth = 5;
       this.cx.lineCap = 'round';
 
       prev = data[i];
-      left += 100;
+      left += toMoveWith;
 
       this.cx.stroke();
     }
