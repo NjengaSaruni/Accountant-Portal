5839e7a859a65c70f75501ec935475349323f130
Peter Njenga <njengasaruni@gmail.com>
2019-01-10 15:15:27 +0300

    Tests for login form and action

--- src/app/modules/auth/components/login-form/login-form.component.spec.ts
+++ src/app/modules/auth/components/login-form/login-form.component.spec.ts
@@ -1,14 +1,18 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 
 import {LoginFormComponent} from './login-form.component';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import {SharedModule} from '../../../shared/shared.module';
-import {StoreModule} from '@ngrx/store';
+import {combineReducers, Store, StoreModule} from '@ngrx/store';
 import {EffectsModule} from '@ngrx/effects';
+import * as fromAuth from '../../store';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
 
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
+  let store: Store<fromAuth.AuthState>;
+  const formBuilder: FormBuilder = new FormBuilder();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,20 +20,65 @@ describe('LoginFormComponent', () => {
       imports: [
         ReactiveFormsModule,
         SharedModule,
-        StoreModule.forRoot({}),
+        StoreModule.forRoot({
+          auth: combineReducers(fromAuth.reducers),
+        }),
         EffectsModule
-      ]
+      ],
+      providers: [
+        FormBuilder
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
-    .compileComponents();
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(LoginFormComponent);
+        component = fixture.componentInstance;
+      });
+
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginFormComponent);
     component = fixture.componentInstance;
+    component.loginForm = formBuilder.group({
+      email: 'john@doe.com',
+      password: 'john!@#'
+    });
     fixture.detectChanges();
   });
 
+  // create reusable function for a dry spec.
+  function updateForm(userEmail, userPassword) {
+    component.loginForm.controls['email'].setValue(userEmail);
+    component.loginForm.controls['password'].setValue(userPassword);
+  }
+
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form value should update from form changes', fakeAsync(() => {
+    updateForm('john@doe.com', 'john!@#');
+    expect(component.loginForm.value).toEqual({
+      email: 'john@doe.com',
+      password: 'john!@#'
+    });
+  }));
+
+  it('should dispatch a login event on submit', () => {
+    updateForm('john@doe.com', 'john!@#');
+    const $event: any = {
+      email: 'john@doe.com',
+      password: 'john!@#'
+    };
+    const action = new fromAuth.Login($event);
+
+    component.signIn();
+
+    expect(store.dispatch).toHaveBeenCalledWith(action);
   });
 });
