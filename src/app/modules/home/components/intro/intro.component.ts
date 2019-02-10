f635847b930ce2c1f573036386076cd95fddfdea
Peter Njenga <njengasaruni@gmail.com>
2019-02-10 15:41:37 +0300

    Platform aware graphs, Color aware backgrounds

--- src/app/modules/home/components/intro/intro.component.ts
+++ src/app/modules/home/components/intro/intro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {LineChart} from '../../../charts/models/LineChart/LineChart';
+import {getMockLinechart} from '../../../shared/utils/randomInt';
+import {WindowRefService} from '../../../shared/services/window-ref.service';
 
 @Component({
   selector: 'app-intro',
@@ -6,8 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./intro.component.scss']
 })
 export class IntroComponent implements OnInit {
+  lineChart: LineChart;
 
-  constructor() { }
+  constructor(private winRef: WindowRefService) {
+    const width = this.winRef.nativeWindow.innerWidth;
+    const chartWidth = width > 600 ? (this.winRef.nativeWindow.innerWidth - 0.1 * this.winRef.nativeWindow.innerWidth)
+      / 2 : this.winRef.nativeWindow.innerWidth;
+    this.lineChart = getMockLinechart(chartWidth);
+    this.lineChart.backgroundColor = '#dad9dc';
+  }
 
   ngOnInit() {
   }
