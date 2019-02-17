b81fa39aca31c5b11a3e4f77aca99240a0e51c20
Peter Njenga <njengasaruni@gmail.com>
2019-02-17 21:14:59 +0300

    Enumeration for months

--- src/app/modules/shared/utils/timeUtils.ts
+++ src/app/modules/shared/utils/timeUtils.ts
@@ -14,3 +14,17 @@ export const FIRST_OF_PREVIOUS_MONTH = new Date(
 
 export const convertToDate = (date: Date) => new Date(date);
 
+export enum Months {
+  Jan = 1,
+  Feb = 2,
+  Mar = 3,
+  Apr = 4,
+  May = 5,
+  Jun = 6,
+  Jul = 7,
+  Aug = 8,
+  Sep = 9,
+  Oct = 10,
+  Nov = 11,
+  Dec = 12
+}
