be97ea36c102138ce61ff0699dde0d6e1274a80a
Peter Njenga <njengasaruni@gmail.com>
2019-04-11 22:27:31 +0300

    Fix initial unit tests

--- src/app/modules/auth/services/auth.guard.service.spec.ts
+++ src/app/modules/auth/services/auth.guard.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthGuardService } from './auth.guard.service';
+import { AuthGuard } from './auth.guard.service';
 
 describe('Auth.GuardService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: Auth.GuardService = TestBed.get(Auth.GuardService);
+    const service: AuthGuard = TestBed.get(AuthGuard);
     expect(service).toBeTruthy();
   });
 });
